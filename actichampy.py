dabad1fc45102edbd53954800ce658ea9789b17f
Mike Lawrence <mike.lwrnc@gmail.com>
2015-03-12 14:18:56 -0300

    Convert indentation from spaces to tabs

--- actichampy.py
+++ actichampy.py
@@ -37,23 +37,23 @@ varying vec4 v_ab;
 
 void main() {
 
-    // Compute the x coordinate from the time index.
-    float x = -1 + 2*a_index.z / (u_n-1);
-    vec2 position = vec2(x, a_position);
+	// Compute the x coordinate from the time index.
+	float x = -1 + 2*a_index.z / (u_n-1);
+	vec2 position = vec2(x, a_position);
 
-    // Find the affine transformation for the subplots.
-    vec2 a = vec2(1., 1./u_nrows)*.9;
-    vec2 b = vec2( -1 + 2 * (a_index.x+.5) ,
-                   -1 + 2 * (a_index.y+.5) / u_nrows );
+	// Find the affine transformation for the subplots.
+	vec2 a = vec2(1., 1./u_nrows)*.9;
+	vec2 b = vec2( -1 + 2 * (a_index.x+.5) ,
+				   -1 + 2 * (a_index.y+.5) / u_nrows );
 
-    // Apply the static subplot transformation + scaling.
-    gl_Position = vec4(a*position+b, 0.0, 1.0);
+	// Apply the static subplot transformation + scaling.
+	gl_Position = vec4(a*position+b, 0.0, 1.0);
 
-    v_index = a_index;
+	v_index = a_index;
 
-    // For clipping test in the fragment shader.
-    v_position = gl_Position.xy;
-    v_ab = vec4(a, b);
+	// For clipping test in the fragment shader.
+	v_position = gl_Position.xy;
+	v_ab = vec4(a, b);
 }
 """
 
@@ -66,104 +66,104 @@ varying vec2 v_position;
 varying vec4 v_ab;
 
 void main() {
-    gl_FragColor = vec4(1.,1.,1.,0.9);
+	gl_FragColor = vec4(1.,1.,1.,0.9);
 
-    // Discard the fragments between the signals (emulate glMultiDrawArrays).
-    if ((fract(v_index.x) > 0.) || (fract(v_index.y) > 0.))
-        discard;
+	// Discard the fragments between the signals (emulate glMultiDrawArrays).
+	if ((fract(v_index.x) > 0.) || (fract(v_index.y) > 0.))
+		discard;
 
-    // Clipping test.
-    vec2 test = abs((v_position.xy-v_ab.zw)/v_ab.xy);
-    if ((test.x > 1) || (test.y > 1))
-        discard;
+	// Clipping test.
+	vec2 test = abs((v_position.xy-v_ab.zw)/v_ab.xy);
+	if ((test.x > 1) || (test.y > 1))
+		discard;
 }
 """
 
 
 class Canvas(app.Canvas):
-    def update_index(self):
-        self.index = np.c_[np.repeat(np.repeat(0, self.nrows), self.samples_per_screen),
-              np.repeat(np.tile(np.arange(self.nrows), 1), self.samples_per_screen),
-              np.tile(np.arange(self.samples_per_screen), self.nrows)].astype(np.float32)
-    def __init__(self,data):
-        self.data = data
-        self.samples_per_screen = self.data.shape[1]
-        self.show_latest = True
-        self.scroll_time = False
-        self.last_sample_to_show = self.data.shape[1]#None
-        self.nrows = self.data.shape[0]
-        self.update_index() #creates self.index
-        app.Canvas.__init__(self, title='Use your wheel to zoom!',keys='interactive')
-        self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
-        self.program['a_position'] = self.data.reshape(-1, 1)
-        self.program['a_index'] = self.index
-        self.program['u_nrows'] = self.nrows
-        self.program['u_n'] = self.samples_per_screen
-        gloo.set_state(clear_color='black', blend=True,
-                       blend_func=('src_alpha', 'one_minus_src_alpha'))
-        gl.glEnable(gl.GL_BLEND)
-        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-        gl.glEnable( gl.GL_LINE_SMOOTH )
-        gl.glHint( gl.GL_LINE_SMOOTH_HINT, gl.GL_NICEST )
+	def update_index(self):
+		self.index = np.c_[np.repeat(np.repeat(0, self.nrows), self.samples_per_screen),
+			  np.repeat(np.tile(np.arange(self.nrows), 1), self.samples_per_screen),
+			  np.tile(np.arange(self.samples_per_screen), self.nrows)].astype(np.float32)
+	def __init__(self,data):
+		self.data = data
+		self.samples_per_screen = self.data.shape[1]
+		self.show_latest = True
+		self.scroll_time = False
+		self.last_sample_to_show = self.data.shape[1]#None
+		self.nrows = self.data.shape[0]
+		self.update_index() #creates self.index
+		app.Canvas.__init__(self, title='Use your wheel to zoom!',keys='interactive')
+		self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
+		self.program['a_position'] = self.data.reshape(-1, 1)
+		self.program['a_index'] = self.index
+		self.program['u_nrows'] = self.nrows
+		self.program['u_n'] = self.samples_per_screen
+		gloo.set_state(clear_color='black', blend=True,
+					blend_func=('src_alpha', 'one_minus_src_alpha'))
+		gl.glEnable(gl.GL_BLEND)
+		gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+		gl.glEnable( gl.GL_LINE_SMOOTH )
+		gl.glHint( gl.GL_LINE_SMOOTH_HINT, gl.GL_NICEST )
 
-    def on_resize(self, event):
-        self.width, self.height = event.size
-        gloo.set_viewport(0, 0, self.width, self.height)
-    def on_key_press(self, event):
-        if event.key.name=='Space': #toggle show_latest on/off
-            self.show_latest = not self.show_latest
-            self.last_sample_to_show = self.data.shape[1]
-        elif event.key.name=='Z': #toggle scoll_time on
-            self.scroll_time = True
-        else:
-            print event.key.name
-    def on_key_release(self, event):
-        if event.key.name=='Z': #toggle scoll_time back off
-            self.scroll_time = False
-    def on_mouse_wheel(self, event):
-        if not self.scroll_time:
-            dy = np.sign(event.delta[1]) * .05
-            self.samples_per_screen = int(self.samples_per_screen * math.exp(2.5*dy))
-            if self.samples_per_screen<100:
-                self.samples_per_screen = 100
-            elif self.samples_per_screen>self.data.shape[1]:
-                self.samples_per_screen = self.data.shape[1]
-        elif not self.show_latest:
-            self.last_sample_to_show = self.last_sample_to_show + self.samples_per_screen*(event.delta[0]/100)
-            if self.last_sample_to_show < self.samples_per_screen:
-                self.last_sample_to_show = self.samples_per_screen
-            elif self.last_sample_to_show > self.data.shape[1]:
-                self.last_sample_to_show = self.data.shape[1]
-    def on_draw(self, event):
-        """Add some data at the end of each signal (real-time signals)."""
-        k = int(1000/60) #emulating 1kHz new data rate (assuming this function gets called every refresh at 60Hz refresh rate)
-        self.data = np.c_[self.data,amplitudes * np.random.randn(self.nrows, k)]
-        print [self.data.shape,self.samples_per_screen,self.show_latest,self.last_sample_to_show]
-        if self.samples_per_screen > self.data.shape[1]:
-            self.samples_per_screen = self.data.shape[1]
-            this_data = self.data
-        else:
-            if self.show_latest:
-                this_data = self.data[:,-self.samples_per_screen:]
-                self.last_sample_to_show = self.data.shape[1]
-            else:
-                if self.samples_per_screen > self.last_sample_to_show:
-                    this_data = self.data[:,0:self.samples_per_screen]
-                else:
-                    this_data = self.data[:,( self.last_sample_to_show - self.samples_per_screen ):self.last_sample_to_show]
-        self.update_index()
-        self.program['a_index'] = self.index
-        self.program['u_n'] = self.samples_per_screen
-        self.program['a_position'] = this_data.reshape(-1, 1).astype(np.float32)
-        self.update()
-        gloo.clear()
-        self.program.draw('line_strip')
+	def on_resize(self, event):
+		self.width, self.height = event.size
+		gloo.set_viewport(0, 0, self.width, self.height)
+	def on_key_press(self, event):
+		if event.key.name=='Space': #toggle show_latest on/off
+			self.show_latest = not self.show_latest
+			self.last_sample_to_show = self.data.shape[1]
+		elif event.key.name=='Z': #toggle scoll_time on
+			self.scroll_time = True
+		else:
+			print event.key.name
+	def on_key_release(self, event):
+		if event.key.name=='Z': #toggle scoll_time back off
+			self.scroll_time = False
+	def on_mouse_wheel(self, event):
+		if not self.scroll_time:
+			dy = np.sign(event.delta[1]) * .05
+			self.samples_per_screen = int(self.samples_per_screen * math.exp(2.5*dy))
+			if self.samples_per_screen<100:
+				self.samples_per_screen = 100
+			elif self.samples_per_screen>self.data.shape[1]:
+				self.samples_per_screen = self.data.shape[1]
+		elif not self.show_latest:
+			self.last_sample_to_show = self.last_sample_to_show + self.samples_per_screen*(event.delta[0]/100)
+			if self.last_sample_to_show < self.samples_per_screen:
+				self.last_sample_to_show = self.samples_per_screen
+			elif self.last_sample_to_show > self.data.shape[1]:
+				self.last_sample_to_show = self.data.shape[1]
+	def on_draw(self, event):
+		"""Add some data at the end of each signal (real-time signals)."""
+		k = int(1000/60) #emulating 1kHz new data rate (assuming this function gets called every refresh at 60Hz refresh rate)
+		self.data = np.c_[self.data,amplitudes * np.random.randn(self.nrows, k)]
+		print [self.data.shape,self.samples_per_screen,self.show_latest,self.last_sample_to_show]
+		if self.samples_per_screen > self.data.shape[1]:
+			self.samples_per_screen = self.data.shape[1]
+			this_data = self.data
+		else:
+			if self.show_latest:
+				this_data = self.data[:,-self.samples_per_screen:]
+				self.last_sample_to_show = self.data.shape[1]
+			else:
+				if self.samples_per_screen > self.last_sample_to_show:
+					this_data = self.data[:,0:self.samples_per_screen]
+				else:
+					this_data = self.data[:,( self.last_sample_to_show - self.samples_per_screen ):self.last_sample_to_show]
+		self.update_index()
+		self.program['a_index'] = self.index
+		self.program['u_n'] = self.samples_per_screen
+		self.program['a_position'] = this_data.reshape(-1, 1).astype(np.float32)
+		self.update()
+		gloo.clear()
+		self.program.draw('line_strip')
 
 if __name__ == '__main__':
-    nrows = 10 #32 channels
-    n = 10000 #start with 10s of data
-    amplitudes = .1 + .2 * np.random.rand(nrows, 1).astype(np.float32)
-    y = amplitudes * np.random.randn(nrows, n).astype(np.float32)
-    c = Canvas(data=y)
-    c.show()
-    app.run()
+	nrows = 10 #32 channels
+	n = 10000 #start with 10s of data
+	amplitudes = .1 + .2 * np.random.rand(nrows, 1).astype(np.float32)
+	y = amplitudes * np.random.randn(nrows, n).astype(np.float32)
+	c = Canvas(data=y)
+	c.show()
+	app.run()
